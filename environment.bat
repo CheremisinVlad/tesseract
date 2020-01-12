d083264d94ae5052785c16c23ef03683e3fb2754
cheremisinVlad <cheremisinvladw@gmail.com>
2020-01-12 15:46:44 +0300

    tesseract service scaffold

---
+++ environment.bat
@@ -0,0 +1,8 @@
+SET ACTIVE_PROFILE=dev
+SET PATH="C:\apache-maven-3.6.3-bin\bin";%PATH%
+
+
+SET EXTERNAL_DB_URL=jdbc:postgresql://localhost:5432/tesseract
+SET DB_URL=jdbc:postgresql://localhost:5432/tesseract
+SET DB_USER=postgres
+SET DB_PASS=admin
